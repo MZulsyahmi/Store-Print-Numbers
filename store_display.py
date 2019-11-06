d0cab6191e7abe4b082891b3a59f4ac3a71759e7
Zulsyahmi Othman <44058064+MZulsyahmi@users.noreply.github.com>
2019-11-06 17:27:12 +0800

    Update store_display.py

--- store_display.py
+++ store_display.py
@@ -1,6 +1,7 @@
 from random import uniform
 import time
 
+#store up to 30 numbers
 num_store = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -8,8 +9,11 @@ num_store = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 while True:
     num_random = uniform(0, 100.00)
     num_store.append(float(num_random))
+    
+    #print latest number
     print("Value: {:.2f}    ".format(num_store[-1]), end='')
     
+    #print number 10 sec before
     num_10sec = num_store[-10]
     print("Value 10sec: {:.2f}".format(num_10sec))
     time.sleep(1)   #delay for 1 sec
